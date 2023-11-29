e27e86c8a228b699b110afadd8a18db37d762c35
David Im <knighthell@gmail.com>
2023-11-29 16:48:58 +0900

    fix: fixed no applied ts_proto_opt when protoc ts,nestjs

--- domain/place/.protoc-shells/protoc_place_nestjs.sh
+++ domain/place/.protoc-shells/protoc_place_nestjs.sh
@@ -4,17 +4,13 @@ PROTO_DIR_PATH=$(dirname "$SHELL_DIR_PATH")
 echo SHELL_DIR_PATH: ${SHELL_DIR_PATH}
 echo PROTO_DIR_PATH: ${PROTO_DIR_PATH}
 
-TS_PROTO_OPTIONS_ARRAY=(
-  "--ts_proto_opt=nestJs=true"
-  "--ts_proto_opt=useOptionals=messages"
-  "--ts_proto_opt=useDate=true"
-  "--ts_proto_opt=env=node"
-  "--ts_proto_opt=addNestjsRestParameter=true"
-  "--ts_proto_opt=unrecognizedEnum=false"
-)
-
 protoc -I "${PROTO_DIR_PATH%%/}" \
     --plugin=./node_modules/.bin/protoc-gen-ts_proto.cmd \
-    "${TS_PROTO_OPTIONS_ARRAY[*]}" \
+    --ts_proto_opt=nestJs=true \
+    --ts_proto_opt=useOptionals=messages \
+    --ts_proto_opt=useDate=true \
+    --ts_proto_opt=env=node \
+    --ts_proto_opt=addNestjsRestParameter=true \
+    --ts_proto_opt=unrecognizedEnum=false \
     --ts_proto_out="${PROTO_DIR_PATH%%/}"/nestjs/ \
     "${PROTO_DIR_PATH%%/}"/**.proto
