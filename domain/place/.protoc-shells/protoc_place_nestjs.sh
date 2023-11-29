668873c67233ff7b293b00bad1df4702ddfd48da
David Im <knighthell@gmail.com>
2023-11-29 17:05:10 +0900

    feat: added ts_proto_opt for grpc-metadata

--- domain/place/.protoc-shells/protoc_place_nestjs.sh
+++ domain/place/.protoc-shells/protoc_place_nestjs.sh
@@ -6,11 +6,12 @@ echo PROTO_DIR_PATH: ${PROTO_DIR_PATH}
 
 protoc -I "${PROTO_DIR_PATH%%/}" \
     --plugin=./node_modules/.bin/protoc-gen-ts_proto.cmd \
-    --ts_proto_opt=nestJs=true \
     --ts_proto_opt=useOptionals=messages \
     --ts_proto_opt=useDate=true \
     --ts_proto_opt=env=node \
-    --ts_proto_opt=addNestjsRestParameter=true \
     --ts_proto_opt=unrecognizedEnum=false \
+    --ts_proto_opt=nestJs=true \
+    --ts_proto_opt=addNestjsRestParameter=true \
+    --ts_proto_opt=addGrpcMetadata=true \
     --ts_proto_out="${PROTO_DIR_PATH%%/}"/nestjs/ \
     "${PROTO_DIR_PATH%%/}"/**.proto
