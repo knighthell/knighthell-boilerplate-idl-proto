711be0b1ab2ebe26a1f6a02d90b46f0967dd8446
David Im <knighthell@gmail.com>
2024-01-24 10:50:14 +0900

    feat: added common domain and changed place service structure

--- domain/place/.protoc-shells/protoc_place_nestjs.sh
+++ domain/place/.protoc-shells/protoc_place_nestjs.sh
@@ -1,10 +1,12 @@
 SHELL_DIR_PATH=$(dirname "$0")
 PROTO_DIR_PATH=$(dirname "$SHELL_DIR_PATH")
+COMMON_DIR_PATH=$(dirname "$PROTO_DIR_PATH")/common
 
-echo SHELL_DIR_PATH: ${SHELL_DIR_PATH}
-echo PROTO_DIR_PATH: ${PROTO_DIR_PATH}
+#echo SHELL_DIR_PATH: ${SHELL_DIR_PATH}
+#echo PROTO_DIR_PATH: ${PROTO_DIR_PATH}
+#echo COMMON_DIR_PATH: ${COMMON_DIR_PATH}
 
-protoc -I "${PROTO_DIR_PATH%%/}" \
+protoc -I "${PROTO_DIR_PATH%%/}" -I "${COMMON_DIR_PATH}"\
     --plugin=./node_modules/.bin/protoc-gen-ts_proto.cmd \
     --ts_proto_opt=useOptionals=none \
     --ts_proto_opt=useDate=true \
@@ -15,3 +17,5 @@ protoc -I "${PROTO_DIR_PATH%%/}" \
     --ts_proto_opt=stringEnums=true \
     --ts_proto_out="${PROTO_DIR_PATH%%/}"/nestjs/ \
     "${PROTO_DIR_PATH%%/}"/**.proto
+
+echo "Compiled Success"
